cb99c31cf2d94e7635a2cac6c0179412bca22760
Yash Londhe <99034957+yashlondhe90960@users.noreply.github.com>
2023-06-28 23:05:11 +0530

    Update Reverse.java

--- src/com/yash/Reverse.java
+++ src/com/yash/Reverse.java
@@ -9,11 +9,12 @@ public class Reverse {
         int rem = n % 10;
         sum = sum * 10 + rem;
         rev1(n/10);
+
+        
     }
 
     static int rev2(int n) {
-        // sometimes you might need some additional variables in the argument
-        // in that case, make another function
+       
         int digits = (int)(Math.log10(n)) + 1;
         return helper(n, digits);
     }
